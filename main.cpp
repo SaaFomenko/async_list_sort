d7da668155ee1d5271a6b457fbf73a9b2b131e36
SaaFomenko <s.fomenko@list.ru>
2023-09-19 11:37:32 +0300

    Issue done.

--- main.cpp
+++ main.cpp
@@ -43,11 +43,14 @@ namespace my
 
 	void async_sort(std::list<int>& l)
 	{
+		//std::promise<std::list<int>::iterator> prom;
+		//std::future<std::list<int>::iterator> ft_res = prom.get_future();
+
 		for (std::list<int>::iterator it = l.begin(); it != l.end(); ++it)
 		{
 			std::promise<std::list<int>::iterator> prom;
 			std::future<std::list<int>::iterator> ft_res = prom.get_future();
-			std::future<void> ft = std::async(async_find_it_min, std::ref(l), l.begin(), std::move(prom));
+			std::future<void> ft = std::async(async_find_it_min, std::ref(l), it, std::move(prom));
 			//std::list<int>::iterator min_val_it = find_it_min(l, it); 
 			std::swap(*it, *ft_res.get());
 		}	
@@ -59,21 +62,29 @@ int main()
 	std::list<int> l1{7, 89, 3, 5, 67, 8, 1, 9, 23, 55};
 	std::list<int> l2{7, 89, 3, 5, 67, 8, 1, 9, 23, 55};
 
+	auto start = std::chrono::steady_clock::now();
 	my::sort(l1);
+	auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
 
 	for (int el : l1)
 	{
 		std::cout << el << ' ';
 	}
 	std::cout << '\n';
+	std::cout << "Time elapsed: " << elapsed_seconds.count() << "s\n\n";
 
+	start = std::chrono::steady_clock::now();
 	my::async_sort(l2);
+	end = std::chrono::steady_clock::now();
+    elapsed_seconds = end - start;
 
 	for (int el : l2)
 	{
 		std::cout << el << ' ';
 	}
 	std::cout << '\n';
+	std::cout << "Time elapsed: " << elapsed_seconds.count() << "s\n\n";
 
 	return 0;
 }
